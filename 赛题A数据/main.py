69e28ad4da9a5d309c12f954a7c415ac1c4f9ac6
Succioi <82268984+Succioi@users.noreply.github.com>
2023-11-03 18:35:47 +0800

    Delete 赛题A数据 directory

--- 赛题A数据/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from keras.models import load_model
-
-
-model = load_model('train/weight.h5')
-data_test = pd.read_csv('data_data.csv')
-data_test = data_test.loc[:,['ent_type','registered_fund']]
-
