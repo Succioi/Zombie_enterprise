f4bbc37a87e38144705492e754a7c0730836a144
Succioi <82268984+Succioi@users.noreply.github.com>
2023-11-03 03:32:54 -0700

    Add files via upload

---
+++ A topic/test/recignizer.py
@@ -0,0 +1,6 @@
+from keras.models import load_model
+
+
+def load_action_premodel(model):
+    return load_model(model)
+
---
+++ A topic/test/test_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+def Test(Enum):
+    """Test Enum"""
+    true = 1;
+    false = 0;
