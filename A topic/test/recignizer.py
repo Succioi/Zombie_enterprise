0a1e074390c3240c80c570e5393d09a1d2ca0e28
Succioi <82268984+Succioi@users.noreply.github.com>
2023-11-13 21:23:07 +0800

    Delete A topic directory

--- A topic/test/recignizer.py
+++
@@ -1,6 +0,0 @@
-from keras.models import load_model
-
-
-def load_action_premodel(model):
-    return load_model(model)
-
--- A topic/test/test_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-def Test(Enum):
-    """Test Enum"""
-    true = 1;
-    false = 0;
